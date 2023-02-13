87b0e58127942383efebef97fe3939fc2577b5e4
kenf1 <103538771+kenf1@users.noreply.github.com>
2023-02-13 14:41:00 -0600

    Add test scipts

---
+++ TestScript_P.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+link = "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-01-10/PFW_2021_public.csv"
+
+df = pd.read_csv(link)
---
+++ TestScript_R.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+
+link <- "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-01-10/PFW_2021_public.csv"
+
+df_r <- read_csv(link)
